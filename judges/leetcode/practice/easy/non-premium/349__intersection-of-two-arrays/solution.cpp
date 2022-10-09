473a1b19faf14091f20bd8898949ffd4ac532360
jayaprakash-a <akulajayaprakash@gmail.com>
2022-10-09 18:36:31 +0530

    Add files via upload

--- judges/leetcode/practice/easy/non-premium/349__intersection-of-two-arrays/solution.cpp
+++ judges/leetcode/practice/easy/non-premium/349__intersection-of-two-arrays/solution.cpp
@@ -43,9 +43,9 @@ namespace Solution_01 {
  *
  * Judge metadata:
  *  - Submission link:
- *  - Status: .  /  test cases passed.
- *  - Runtime:  ms, faster than % of C++ online submissions
- *  - Memory usage:  MB, less than % of C++ online submissions
+ *  - Status: Accepted. 55 / 55 test cases passed.
+ *  - Runtime: 7 ms, faster than % of C++ online submissions
+ *  - Memory usage: 10.5 MB, less than % of C++ online submissions
  * Algorithm metadata:
  *  - Time complexity: O(t*n^2), where t is number of tests and n is size of nums
  *  - Space complexity: O(1)
