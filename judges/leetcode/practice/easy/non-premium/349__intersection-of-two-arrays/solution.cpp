a55efdf95cd09e10c1426450f2b8b969e7abde4e
jayaprakash-a <akulajayaprakash@gmail.com>
2022-10-09 10:29:59 +0530

    Add files via upload
    Added code for intersection of two arrays question

---
+++ judges/leetcode/practice/easy/non-premium/349__intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,81 @@
+// clang-format off
+/**
+ * COPYRIGHT NOTICE
+ *
+ * Copyright (c) 2022. Jayaprakash Akula <akulajayaprakash@gmail.com>
+ *
+ * This file is created for LEETCODE Practice in competitive programming.
+ *
+ * This file cannot be copied and/or distributed without the express
+ * permission of the copyright owner.
+ *
+ */
+
+/**
+ * FILE DESCRIPTION
+ *
+ *        Filename: 349__intersection-of-two-arrays/solution.cpp
+ *      Created on: 09 October 2022
+ *   Last modified: 09 October 2022
+ *          Author: jayaprakash-a
+ *     Description: LEETCODE submission for 'Intersection of Two Arrays' problem
+ *     Is Premium?: No
+ */
+
+/**
+ * CHANGELOG
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 09-10-2022                jayaprakash-a      - Creation of file
+ */
+// clang-format on
+
+#include <unordered_set>
+#include <vector>
+
+using std::unordered_set;
+using std::vector;
+
+namespace Solution_01 {
+// clang-format off
+/**
+ * CLASS DESCRIPTION
+ *
+ * Judge metadata:
+ *  - Submission link:
+ *  - Status: .  /  test cases passed.
+ *  - Runtime:  ms, faster than % of C++ online submissions
+ *  - Memory usage:  MB, less than % of C++ online submissions
+ * Algorithm metadata:
+ *  - Time complexity: O(t*n^2), where t is number of tests and n is size of nums
+ *  - Space complexity: O(1)
+ *  - Tags:
+ *  - Categories:
+ * Additional notes:
+ *  -
+ */
+// clang-format on
+class Solution {
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity:
+     * Space complexity:
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> numSet(nums1.begin(), nums1.end());
+        vector<int>        res;
+        for (auto num : nums2)
+            if (numSet.find(num) != numSet.end()) {
+                res.push_back(num);
+                numSet.erase(num);
+            }
+        return res;
+    }
+};
+}  // namespace Solution_01
